6c5e1f7f24a6a6c34bf875500339c7a47d8bf796
Lou Jackson <jack@uofc.xyz>
2023-07-28 18:22:31 -0400

    Changed some verbage

--- git-sync.sh
+++ git-sync.sh
@@ -1,5 +1,4 @@
-remotes='origin lab vps'
-# local
+remotes="origin lab vps"
 
 dotfiles="$HOME/git/dotfiles.git"
 config="/usr/bin/git --git-dir=$dotfiles --work-tree=$HOME"
@@ -11,13 +10,11 @@ if
             echo "Dotfiles repo present."
 fi
 
-dotf_sync () {
              $config pull origin master
 for r in $remotes
          do
-             $config push $r master
+             $config push $r master&
 done
-}
 
 www="$HOME/git/uofc" # Website directory.
 org="$HOME/documents/org" # For notes, and miscellanious org documents.
@@ -25,8 +22,6 @@ scripts="$HOME/.local/bin/" # For general scripts.
 gsync="$HOME/.local/bin/git-sync" # For my git-sync script
 dirs="$www $org $scripts $gsync"
 
-sync () {
 for d in $( echo $dirs );
 do
-    git -C $d pull origin master;
-}
+    git -C $d pull origin master&
