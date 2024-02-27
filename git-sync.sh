984e74201a7ccc13ae0b96e8f3d6877e40950a6c
Lou Jackson <jack@uofc.xyz>
2024-02-27 04:08:14 +0000

    Added comments before each action

--- git-sync.sh
+++ git-sync.sh
@@ -1,19 +1,30 @@
+#!/bin/bash
+
+# Set $remotes variable
 remotes="origin"
 
+# Set dotfiles variable
 dotfiles="$HOME/git/dotfiles.git"
+
+# Set config command variable
 config="/usr/bin/git --git-dir=$dotfiles --work-tree=$HOME"
+
+# If statement to check for dotfiles directory. If directory is not present, one will be created
 if
     [ ! -d "$dotfiles" ];
     then
         git init --bare $dotfiles
 fi
 
+# For loop to sync dotfiles
 for r in $remotes
          do
              $config pull $r master&&
              $config push $r master
 done
 
+# Set remaining directory variables.
+# Will possibly set up a loop for this in the future.
 uofc="$HOME/git/uofc" # Website directory.
 decades="$HOME/git/decades" # Website directory.
 org="$HOME/git/org" # For notes, and miscellanious org documents.
@@ -21,6 +32,7 @@ scripts="$HOME/git/scripts/" # For general scripts.
 gsync="$HOME/git/git_sync" # For my git-sync script
 dirs="$uofc $decades $org $scripts $gsync"
 
+# For loop to sync remaining directories with remote hosts
 for d in $( echo $dirs );
 do
     git -C $d pull $remotes master&&
