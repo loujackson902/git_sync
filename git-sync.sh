2458149a943c7887b3ffc1536816806c8251ed09
Lou Jackson <jack@uofc.xyz>
2024-08-20 21:05:11 -0400

    Modified formatting

--- git-sync.sh
+++ git-sync.sh
@@ -1,54 +1,31 @@
-#!/bin/bash
+remotes="origin lab"
 
-# Set $remotes variable
-remotes="origin"
-echo '$origin set'
+#vps
 
-# Set dotfiles variable
 dotfiles="$HOME/git/dotfiles.git"
-echo '$dotfiles set'
-
-# Set config command variable
 config="/usr/bin/git --git-dir=$dotfiles --work-tree=$HOME"
-echo '$config set'
-
-# If statement to check for dotfiles directory. If directory is not present, one will be created
 if
     [ ! -d "$dotfiles" ];
     then
         git init --bare $dotfiles
-        echo '\$dotfiles not found, creating initializing git bare directory'
 fi
 
-# For loop to sync dotfiles
+             $config pull origin master
 for r in $remotes
          do
              $config pull $r master&&
              $config push $r master
 done
 
-# Set remaining directory variables.
-# Will possibly set up a loop for this in the future.
-uofc="$HOME/git/uofc" # Website directory.
-echo '$uofc set'
-decades="$HOME/git/decades" # Website directory.
-echo '$decades set'
-org="$HOME/git/org" # For notes, and miscellanious org documents.
-echo '$org set'
-scripts="$HOME/git/scripts/" # For general scripts.
-echo '$scripts set'
-gsync="$HOME/git/git_sync" # For my git-sync script
-echo '$gsync set'
-dirs="$uofc $decades $org $scripts $gsync"
-echo 'Directory variables created'
+www="$HOME/git/uofc" # Website directory.
+org="$HOME/documents/org" # For notes, and miscellanious org documents.
+scripts="$HOME/.local/bin/" # For general scripts.
+gsync="$HOME/.local/bin/git-sync" # For my git-sync script
+dirs="$www $org $scripts $gsync"
 
-# For loop to sync remaining directories with remote hosts
 for d in $( echo $dirs );
 do
-    git -C $d pull $remotes master&&
-    git -C $d push $remotes master
-done
+    git -C $d pull origin master&&
+    git -C $d push origin master
 
-# Clear previously set variables
-unset remotes dotfiles config r uofc decades org scripts gsync dirs;
-echo "Cleared variables"
+done
